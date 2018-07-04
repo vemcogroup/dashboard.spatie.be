27d5e40491a5b2b5f07f30aeb03218143c571382
Martin Kiforenko Jensby <mkj@vemcount.com>
2018-07-04 09:46:37 +0200

    Regit to Gitlab tasks

--- resources/assets/js/components/Tasks.vue
+++ resources/assets/js/components/Tasks.vue
@@ -2,7 +2,14 @@
     <tile :position="position" modifiers="overflow">
         <section class="github-file">
             <h1 class="github-file__title">{{ teamMember }}</h1>
-            <div class="github-file__content" v-html="tasks"></div>
+            <div class="github-file__content" v-for="task in tasks">
+                <ul>
+                    <li class="statistic" style="justify-content: inherit;">
+                        <span class="statistic__count" style="margin-right: 5px;">#{{ task.id }}</span>
+                        <span class="statistic__label">{{ task.title }}</span>
+                    </li>
+                </ul>
+            </div>
         </section>
     </tile>
 </template>
