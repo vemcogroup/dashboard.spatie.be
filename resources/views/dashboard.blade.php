34ab8c1601f4e45ebcbbc14a714c28bad6827280
Martin Kiforenko Jensby <mkj@vemcount.com>
2018-07-04 09:47:25 +0200

    Replace Github in favor of Gitlab, change team-members username

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,10 +9,10 @@
     <uptime position="a1:a3"></uptime>
     <packagist position="b1"></packagist>
     <npm position="b2"></npm>
-    <github position="b3"></github>
+    <gitlab position="b3"></gitlab>
     <music position="d1"></music>
-    <tasks team-member="dk" position="c1:c3"></tasks>
-    <tasks team-member="mk" position="d1:d3"></tasks>
+    <tasks team-member="dkralj" position="c1:c3"></tasks>
+    <tasks team-member="makije" position="d1:d3"></tasks>
     <time-weather position="e1" date-format="ddd DD/MM" time-zone="Europe/Brussels" weather-city="Fredericia"></time-weather>
     <api-status position="e2:e3"></api-status>
     <internet-connection></internet-connection>
