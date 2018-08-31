a47c21b996221712f3726bfca3fa9011fdbb1153
Henrik B Hansen <hbh@vemcount.com>
2018-08-31 10:13:08 +0200

    Updated dashboard layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,8 +7,8 @@
 <dashboard id="dashboard" columns="6" rows="8">
     <twitter :initial-tweets="{{ json_encode($initialTweets) }}" position="a1:a7"></twitter>
     <gitlab-labels label="To Do" :show-time="false" :show-assignees="true" order-by="weight" position="b1:c7"></gitlab-labels>
-    <gitlab-labels label="Implementing solution" :show-time="true" order-by="weight" :show-assignees="true" position="d1:e4"></gitlab-labels>
-    <gitlab-milestones position="d5:e7"></gitlab-milestones>
+    <gitlab-labels label="Implementing solution" :show-time="true" order-by="weight" :show-assignees="true" position="d1:e5"></gitlab-labels>
+    <gitlab-milestones position="d6:e7"></gitlab-milestones>
     <time-weather position="f1:f3" date-format="ddd DD/MM" time-zone="Europe/Copenhagen" weather-city="Fredericia"></time-weather>
     <stats position="f4:f8"></stats>
     <rss-feed position="a8:e8"></rss-feed>
