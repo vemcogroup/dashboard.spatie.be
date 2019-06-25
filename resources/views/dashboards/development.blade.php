ad4a5ffb2a4d37200bbdb632dcfcb80ab9bdc682
Henrik B Hansen <hbh@vemcount.com>
2019-06-25 13:01:09 +0200

    Removed RSS

--- resources/views/dashboards/development.blade.php
+++ resources/views/dashboards/development.blade.php
@@ -12,15 +12,14 @@
 <div id="dashboard">
     <dashboard class="font-sans">
         <twitter position="a1:a21" :initial-tweets="{{ json_encode($initialTweets) }}"></twitter>
-        <dev-services position="a1:a21"></dev-services>
+        <dev-services position="a1:a24"></dev-services>
         <time-weather position="f1:f6" date-format="ddd DD/MM" time-zone="Europe/Copenhagen" weather-city="Fredericia"></time-weather>
         <internet-connection position="f1:f6"></internet-connection>
-        <gitlab-labels position="b1:c21" label="To Do" :show-time="false" :show-assignees="true" order-by="weight"></gitlab-labels>
+        <gitlab-labels position="b1:c24" label="To Do" :show-time="false" :show-assignees="true" order-by="weight"></gitlab-labels>
         <gitlab-labels position="d1:e16" label="Implementing solution" :show-time="false" order-by="weight" :show-assignees="true"></gitlab-labels>
-        <gitlab-labels position="d17:e21" label="Bug patrol" :show-time="false" :show-assignees="true" order-by="weight"></gitlab-labels>
+        <gitlab-labels position="d17:e24" label="Bug patrol" :show-time="false" :show-assignees="true" order-by="weight"></gitlab-labels>
         <stats position="f7:f17"></stats>
         <statistics position="f18:f24"></statistics>
-        <rss-feed position="a22:e24"></rss-feed>
     </dashboard>
 </div>
 
