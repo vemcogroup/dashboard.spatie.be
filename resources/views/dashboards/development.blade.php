644a6a08928b1afdd2956054df7fffcf06a63a53
Henrik B Hansen <hbh@vemcount.com>
2019-08-15 11:47:47 +0200

    Moved bug patrol below TODO

--- resources/views/dashboards/development.blade.php
+++ resources/views/dashboards/development.blade.php
@@ -15,9 +15,9 @@
         <dev-services position="a1:a24"></dev-services>
         <time-weather position="f1:f6" date-format="ddd DD/MM" time-zone="Europe/Copenhagen" weather-city="Fredericia"></time-weather>
         <internet-connection position="f1:f6"></internet-connection>
-        <gitlab-labels position="b1:c24" label="To Do" :show-time="false" :show-assignees="true" order-by="weight"></gitlab-labels>
-        <gitlab-labels position="d1:e16" label="Implementing solution" :show-time="false" order-by="weight" :show-assignees="true"></gitlab-labels>
-        <gitlab-labels position="d17:e24" label="Bug patrol" :show-time="false" :show-assignees="true" order-by="weight"></gitlab-labels>
+        <gitlab-labels position="b1:c16" label="To Do" :show-time="false" :show-assignees="true" order-by="weight"></gitlab-labels>
+        <gitlab-labels position="b17:c24" label="Bug patrol" :show-time="false" :show-assignees="true" order-by="weight"></gitlab-labels>
+        <gitlab-labels position="d1:e24" label="Implementing solution" :show-time="false" order-by="weight" :show-assignees="true"></gitlab-labels>
         <stats position="f7:f17"></stats>
         <statistics position="f18:f24"></statistics>
     </dashboard>
